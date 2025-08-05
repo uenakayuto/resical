e4a2c2d5e40f4f49ba20264bdaa9500e237a8596
Uenaka Yuto <uenaka.yuto224@gmail.com>
2025-08-05 13:08:55 +0900

    Add endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from itertools import combinations
 import threading
 import time
 import queue
+import math
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -20,16 +21,38 @@ class CombinationResponse(BaseModel):
     message: Optional[str] = None
     execution_time: Optional[float] = None
 
+def validate_inputs(numbers: List[int], target: int, result_queue):
+    if target < 1 or not isinstance(target, int):
+        result_queue.put(CombinationResponse(message="targetは1以上の自然数である必要があります．"))
+    elif any(n < 1 or not isinstance(n, int) for n in numbers):
+        result_queue.put(CombinationResponse(message="numbersには1以上の自然数のみを含めてください．"))
+
 def find_combination_worker(numbers, target, result_queue):
+    validate_inputs(numbers, target, result_queue)
+    if not result_queue.empty():
+        return
+    sorted_numbers = sorted(numbers, reverse=True)
+    # print(f"Sorted numbers: {sorted_numbers}")
+    max_sorted_numbers = sorted_numbers[0]
+    # print(f"Max sorted number: {max_sorted_numbers}")
+    threshold = math.ceil(target / max_sorted_numbers)
+    if threshold > len(sorted_numbers):
+        result_queue.put(CombinationResponse(message="全合計がtargetに届きません．"))
+        return
+    # print(f"Threshold for combinations: {threshold}")
     best_sum = float('inf')
     best_combination = None
 
-    for r in range(1, len(numbers) + 1):
-        for comb in combinations(numbers, r):
+    for r in range(threshold, len(sorted_numbers) + 1):
+        for comb in combinations(sorted_numbers, r):
+            # print(f"Checking combination: {comb}")
             total = sum(comb)
             if total == target:
                 result_queue.put(CombinationResponse(exact=list(comb)))
                 return
+            if total < target:
+                # print(f"break: {comb}")
+                break
             if target < total < best_sum:
                 best_sum = total
                 best_combination = comb
@@ -72,6 +95,10 @@ def remove_used_numbers(req: RemovalRequest):
         if u in remaining:
             remaining.remove(u)
     return {"remaining": remaining}
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
 
 # CORSミドルウェアの設定
 app.add_middleware(
